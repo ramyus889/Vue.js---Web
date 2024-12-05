b1ee97d9a5b80cb206a5a8ef3292847481a771ce
ramyus889 <ramyus889.com>
2024-12-05 16:06:45 +0300

    макет header сделан

--- src/component/UseHeader.vue
+++ src/component/UseHeader.vue
@@ -1,5 +1,26 @@
 <script setup></script>
 
 <template>
-  <div class="">Frontend</div>
+  <div class="fixed z-50 flex w-full top-4 lg:top-6 place-content-center">
+    <div class="max-w-[1672px] px-6 w-full bg-black py-2 rounded-[14px]">
+      <div class="flex items-center justify-between">
+        <div class="text-4xl">CURSOR</div>
+        <ul class="flex gap-11">
+          <li><a href="#">PRICING</a></li>
+          <li><a href="#">FEATURES</a></li>
+          <li><a href="#">BLOG</a></li>
+          <li><a href="#">FORUM</a></li>
+          <li><a href="#">CAREERS</a></li>
+        </ul>
+        <div class="flex gap-3">
+          <UseButton class="!bg-black !text-white !border !border-slate-700" label="SIGN IN" />
+          <UseButton
+            class="!bg-white !text-black !border !border-black"
+            icon="pi pi-download"
+            label="DOWNLOAD"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
--- src/component/UseHome.vue
+++ src/component/UseHome.vue
@@ -2,6 +2,8 @@
 
 <template>
   <div class="mt-20 ms-20">
-    <div class="">Frontend</div>
+    <div class="my-[2000px]">
+      <div class="bg-white w-full h-[100px]"></div>
+    </div>
   </div>
 </template>
