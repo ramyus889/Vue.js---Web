20fda88980aaed3fcfcf9961dae653e961bb7cbe
ramyus889 <ramyus889.com>
2024-12-05 16:09:59 +0300

    add

--- src/component/UseHome.vue
+++ src/component/UseHome.vue
@@ -1,9 +1,9 @@
 <script setup></script>
 
 <template>
-  <div class="mt-20 ms-20">
-    <div class="my-[2000px]">
-      <div class="bg-white w-full h-[100px]"></div>
+  <div class="flex mt-40 place-content-center">
+    <div class="">
+      <div class="">asd</div>
     </div>
   </div>
 </template>
