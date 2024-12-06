e7316ba288fc690eac0e47300249cfefbf9b18c6
ramyus889 <ramyus889.com>
2024-12-06 13:38:12 +0300

    Main готов

--- src/component/UseHeader.vue
+++ src/component/UseHeader.vue
@@ -4,7 +4,7 @@
   <div class="fixed z-50 flex w-full top-4 lg:top-6 place-content-center">
     <div class="max-w-[1672px] px-6 w-full bg-black py-2 rounded-[14px]">
       <div class="flex items-center justify-between">
-        <div class="text-4xl">CURSOR</div>
+        <div class="text-xl font-bold">CURSOR</div>
         <ul class="flex gap-11">
           <li><a href="#">PRICING</a></li>
           <li><a href="#">FEATURES</a></li>
